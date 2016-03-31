94780a70938d20bf7107e7051125e320d6c646f6
siminpeng <18253590634@163.com>
2016-03-31 22:40:58 +0800

    addinfo.php 连接数据库

--- string.php
+++ string.php
@@ -1,4 +1,17 @@
 <?php
-/**
- * String和正则表达式
- */
+	/**
+	 * String和正则表达式
+	 */
+	/*
+	 * 字符串的格式化
+	 * chop();
+	 * tirm();//去除字符串中的空格
+	 * ltrim();
+	 */	
+	$str="   hello world !    " ;
+	$str = trim($str);
+	echo $str;
+	echo 'end';
+	
+
+ 
